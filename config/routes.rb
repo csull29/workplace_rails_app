83be1f9291b790283c146946e2c3300087914651
EC2 Default User <ec2-user@ip-172-31-34-6.eu-west-1.compute.internal>
2018-08-02 18:46:07 +0000

    creation user/project association

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
 
   resources :user_projects
-  resources :artefacts
+  resources :artifacts
   resources :tenants do
     resources :projects do
-      get 'users', on: :member
-      put 'add_user', on: :member
+      get 'users', on: :member 
+      put 'add_user', on: :member 
+    end
   end
   resources :members
   get 'home/index'
-  root :to => "home#index"
+
+   root :to => "home#index"
+
     
   # *MUST* come *BEFORE* devise's definitions (below)
-  
   as :user do   
     match '/user/confirmation' => 'confirmations#update', :via => :put, :as => :update_user_confirmation
   end
@@ -25,6 +27,7 @@ Rails.application.routes.draw do
   }
   
   match '/plan/edit' => 'tenants#edit', via: :get, as: :edit_plan
-  match 'plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
+  match '/plan/update' => 'tenants#update', via: [:put, :patch], as: :update_plan
+
 
 end
